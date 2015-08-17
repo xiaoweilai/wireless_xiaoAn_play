6df616a9116a3db7014ed12af3956b5af78c1be7
abc <wxjlmr@126.com>
2015-08-18 00:11:41 +0800

    绑定本地ip地址，绑定ip内容，可以正常接收，但是QScreenCapture.exe不行，remote desk transfer是可以的。 QScreenCapture.exe一开启就退出了，什么原因呢？

--- QtAndroid_Screen/QtAndroidScreen/mainwindow.cpp
+++ QtAndroid_Screen/QtAndroidScreen/mainwindow.cpp
@@ -4,13 +4,38 @@
 #include <QHostAddress>
 #include <QProcess>
 #include <QNetworkInterface>
+#include <QMessageBox>
 #include "qDebug2Logcat.h"
+
+#if 1
+//编码汉字
+//#define str_china(A)     QString::fromLocal8Bit(#A)
+#define str_china(A)     QString::fromLocal8Bit(A)
+//#define str_china(A)     QString::fromUtf8(#A)
+#else
+#define str_china(A)     codec->toUnicode(#A)
+#endif
+
+#define DEFAULT_PORT   "16689"
+
+
+#define T1M (1*1024*1024)
+#define T1K (1*1024)
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    TotalBytes = 0;
+    bytesReceived = 0;
+    fileNameSize = 0;
+    bytesNeedRecv = 0;
+    OnlyOneClient = FLAGS_NONE;
+
+
 //    showifconfig();
 //    installLogcatMessageHandler("ScreenCapture");
     if(Getifconfig().contains("192.168"))
@@ -25,7 +50,13 @@ MainWindow::MainWindow(QWidget *parent) :
     }
     qDebug() << "bind current ip addr:" << Getifconfig();
 
+//    start();
+//    connect(ui->startButton, SIGNAL(clicked()), this, SLOT(start()));
+//    connect(ui->stopButton, SIGNAL(clicked()), this, SLOT(stop()));
 
+//    connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
+    connect(&tcpServer, SIGNAL(newConnection()),
+            this, SLOT(acceptConnection()));
 }
 
 MainWindow::~MainWindow()
@@ -78,11 +109,6 @@ void MainWindow::showifconfig(void)
         }
 
     }
-}
-
-void MainWindow::on_pushButton_clicked()
-{
-    Getifconfig();
 }
 
 
@@ -141,3 +167,308 @@ QString MainWindow::Getifconfig(void)
 }
 
 
+void MainWindow::start()
+{
+    ui->startButton->setEnabled(false);
+
+
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    bytesReceived = 0;
+    QHostAddress hostaddr;
+    hostaddr.setAddress(bindIpAddr());
+
+    while (!tcpServer.isListening() &&
+           //           !tcpServer.listen(QHostAddress::LocalHost,16689)) {
+           //           !tcpServer.listen(bindIpAddr(),16689)) {
+           !tcpServer.listen(hostaddr, QString(DEFAULT_PORT).toInt())) {
+
+        QMessageBox::StandardButton ret = QMessageBox::critical(this,
+                                                                str_china("回环"),
+                                                                str_china("无法开始测试: %1.")
+                                                                .arg(tcpServer.errorString()),
+                                                                QMessageBox::Retry
+                                                                | QMessageBox::Cancel);
+        if (ret == QMessageBox::Cancel)
+            return;
+    }
+    //ui->serverStatusLabel->setText(str_china("监听"));
+    ui->label->setText(str_china("监听"));
+    setOnlyOneClient();
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    qDebug() << "on start button clicked!!!";
+    Getifconfig();
+    qDebug() << "starting listen!!" << endl;
+
+    start();
+}
+
+
+void MainWindow::displayError(QAbstractSocket::SocketError socketError)
+{
+    qDebug() <<"find Error!:" << socketError;
+
+
+    tcpServerConnection->close();
+    tcpServer.close();
+//    ui->serverProgressBar->reset();
+    //ui->serverStatusLabel->setText(str_china("服务端就绪"));
+    ui->label->setText(str_china("服务端就绪"));
+    ui->startButton->setEnabled(true);
+
+    TotalBytes = 0;
+    bytesReceived = 0;
+    fileNameSize = 0;
+
+
+
+    QApplication::restoreOverrideCursor();
+
+    QMessageBox::information(this, str_china("网络错误"),
+                             str_china("产生如下错误: %1.")
+                             .arg(tcpServerConnection->errorString()));
+    start();
+}
+
+QString MainWindow::bindIpAddr()
+{
+    return Getifconfig();
+}
+
+quint8 MainWindow::getOnlyOneClient()
+{
+    return OnlyOneClient;
+}
+
+void MainWindow::setOnlyOneClient()
+{
+    OnlyOneClient = FLAGS_HAVE;
+}
+
+
+void MainWindow::acceptConnection()
+{
+    qDebug() <<"creating one new connection....";
+
+    tcpServerConnection = tcpServer.nextPendingConnection();
+    connect(tcpServerConnection, SIGNAL(readyRead()),
+            this, SLOT(updateServerProgress()));
+    connect(tcpServerConnection, SIGNAL(error(QAbstractSocket::SocketError)),
+            this, SLOT(displayError(QAbstractSocket::SocketError)));
+
+//    ui->serverStatusLabel->setText(str_china("接受连接"));
+    ui->label->setText(str_china("接受连接"));
+    tcpServer.close();
+    setOnlyOneClient();
+}
+
+void MainWindow::stop()
+{
+//    tcpServer.close();
+//    tcpServerConnection->close();
+//    ui->serverProgressBar->reset();
+//    ui->serverStatusLabel->setText(str_china("服务端就绪"));
+//    ui->startButton->setEnabled(true);
+
+    TotalBytes = 0;
+    bytesReceived = 0;
+    fileNameSize = 0;
+
+    qDebug() <<"stop!!!!!";
+
+    QApplication::restoreOverrideCursor();
+    ui->startButton->setEnabled(true);
+}
+
+
+
+void MainWindow::updateServerProgress()
+{
+#if 1
+    QDataStream in(tcpServerConnection);
+    in.setVersion(QDataStream::Qt_4_0);
+    //    QTime cTime;
+    //    cTime.start();
+    //    static quint64 etime = 0;
+////0xff 0xfe 0xfd 0xfc
+//#define HEADSIZE  (4)
+//    quint8 headbuf[HEADSIZE];
+
+//    if(bytesReceived <= HEADSIZE)
+//    {
+//        if(tcpServerConnection->bytesAvailable() >= HEADSIZE){
+//            in>>TotalBytes>>fileNameSize;
+//            bytesReceived += sizeof(qint64)*2;
+//            inBlock.resize(0);
+//        }
+//    }
+
+    if(bytesReceived <= sizeof(qint64)*2){
+        if((tcpServerConnection->bytesAvailable() >= sizeof(qint64)*2)&&(fileNameSize ==0)){
+            in>>TotalBytes>>fileNameSize;
+            bytesReceived += sizeof(qint64)*2;
+            inBlock.resize(0);
+        }
+        if((tcpServerConnection->bytesAvailable() >= fileNameSize)&&(fileNameSize !=0)){
+            in>>fileName;
+            qDebug() << "filename:" <<fileName;
+
+            bytesReceived += fileNameSize;
+            //            localFile = new QFile(fileName);
+            //            if (!localFile->open(QFile::WriteOnly )) {
+            //                QMessageBox::warning(this, str_china("应用程序"),
+            //                                     str_china("无法读取文件 %1:\n%2.")
+            //                                     .arg(fileName)
+            //                                     .arg(localFile->errorString()));
+            //                return;
+            //            }
+        }else{
+            return;
+        }
+    }
+
+
+    if (bytesReceived < TotalBytes){
+#if 1
+        /* 实际需要接收的字节数 */
+        bytesNeedRecv = TotalBytes - bytesReceived;
+
+
+
+        bytesReceived += tcpServerConnection->bytesAvailable();
+        //        inBlock = tcpServerConnection->readAll();
+        //        localFile->write(inBlock);
+        if(bytesReceived >= TotalBytes){
+//            tcpServerConnection->setReadBufferSize();
+            inBlock.append(tcpServerConnection->read(bytesNeedRecv));
+            bytesReceived = TotalBytes;
+        }else{
+            inBlock.append(tcpServerConnection->readAll());
+        }
+
+
+#else
+        bytesReceived += tcpServerConnection->bytesAvailable();
+        inBlock = tcpServerConnection->readAll();
+        localFile->write(inBlock);
+        inBlock.resize(0);
+#endif
+        qDebug() << "bytesReceived:"<< bytesReceived;
+        qDebug() << "TotalBytes   :"<< TotalBytes;
+    }
+//    ui->serverProgressBar->setMaximum(TotalBytes);
+//    ui->serverProgressBar->setValue(bytesReceived);
+    //    qDebug()<<"bytesReceived---->>>>>>>>>"<< bytesReceived;
+//    ui->serverStatusLabel->setText(str_china("已接收 %1MB")
+//                                   .arg(bytesReceived / (1024 * 1024)));
+    ui->label->setText(str_china("已接收 %1MB")
+                                   .arg(bytesReceived / (1024 * 1024)));
+
+    if (bytesReceived == TotalBytes) {
+        qDebug() << "recived one !!!";
+#if 0
+        //        tcpServerConnection->close();
+        //        ui->startButton->setEnabled(true);
+
+        //        QByteArray array = socket.read( dataSize );
+        QBuffer buffer(&inBlock);
+        buffer.open( QIODevice::ReadOnly );
+
+        QImageReader reader(&buffer, STREAM_PIC_FORT);
+        QImage image = reader.read();
+
+        if ( !image.isNull() ) {
+            ui->imageLabel->setPixmap( QPixmap::fromImage( image ) );
+            //            ui->imageLabel->setText( tr("") );
+        }
+        else {
+            ui->imageLabel->setText( tr("<i>Invalid image received!</i>") );
+        }
+#endif
+
+        QApplication::restoreOverrideCursor();
+
+        inBlock.resize(0);
+
+        //        localFile->close();
+        TotalBytes = 0;
+        bytesReceived = 0;
+        fileNameSize = 0;
+
+        bytesNeedRecv = 0;
+
+
+
+
+
+        //        qDebug() << "consume time:" << etime;
+        //        qDebug() << "transfer rate:" << (bytesReceived/T1M)/(etime/1000.00)
+        //                 <<"MB/s";
+        //        qDebug() << "transfer rate:" << (bytesReceived/T1K)/(etime/1000)
+        //                 <<"KB/s";
+    }
+    //    etime += cTime.elapsed();
+    //    qDebug() << "consume time:" << etime;
+
+#else
+    QDataStream in(tcpServerConnection);
+    in.setVersion(QDataStream::Qt_4_3);
+    QTime cTime;
+    cTime.start();
+    static quint64 etime = 0;
+
+
+    if(bytesReceived <= sizeof(qint64)*2){
+        if((tcpServerConnection->bytesAvailable() >= sizeof(qint64)*2)&&(fileNameSize ==0)){
+            in>>TotalBytes>>fileNameSize;
+            bytesReceived += sizeof(qint64)*2;
+        }
+        if((tcpServerConnection->bytesAvailable() >= fileNameSize)&&(fileNameSize !=0)){
+            in>>fileName;
+            qDebug() << "filename:" <<fileName;
+
+            bytesReceived += fileNameSize;
+            localFile = new QFile(fileName);
+            if (!localFile->open(QFile::WriteOnly )) {
+                QMessageBox::warning(this, str_china("应用程序"),
+                                     str_china("无法读取文件 %1:\n%2.")
+                                     .arg(fileName)
+                                     .arg(localFile->errorString()));
+                return;
+            }
+        }else{
+            return;
+        }
+    }
+
+
+    if (bytesReceived < TotalBytes){
+        bytesReceived += tcpServerConnection->bytesAvailable();
+        inBlock = tcpServerConnection->readAll();
+        localFile->write(inBlock);
+        inBlock.resize(0);
+    }
+    ui->serverProgressBar->setMaximum(TotalBytes);
+    ui->serverProgressBar->setValue(bytesReceived);
+    qDebug()<<bytesReceived;
+    ui->serverStatusLabel->setText(str_china("已接收 %1MB")
+                                   .arg(bytesReceived / (1024 * 1024)));
+
+    if (bytesReceived == TotalBytes) {
+        tcpServerConnection->close();
+        ui->startButton->setEnabled(true);
+        QApplication::restoreOverrideCursor();
+
+        localFile->close();
+        qDebug() << "consume time:" << etime;
+        qDebug() << "transfer rate:" << (bytesReceived/T1M)/(etime/1000.00)
+                 <<"MB/s";
+        qDebug() << "transfer rate:" << (bytesReceived/T1K)/(etime/1000)
+                 <<"KB/s";
+    }
+    etime += cTime.elapsed();
+    qDebug() << "consume time:" << etime;
+#endif
+}
--- QtAndroid_Screen/QtAndroidScreen/mainwindow.h
+++ QtAndroid_Screen/QtAndroidScreen/mainwindow.h
@@ -2,10 +2,25 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QAbstractSocket>
+#include <QIODevice>
+#include <QFile>
+
+
+enum FLAGS_CLIENT
+{
+    FLAGS_NONE = 0,
+    FLAGS_HAVE
+};
 
 namespace Ui {
 class MainWindow;
 }
+
+
+
 
 class MainWindow : public QMainWindow
 {
@@ -21,9 +36,31 @@ private slots:
     void on_pushButton_clicked();
     void showifconfig(void);
     QString Getifconfig(void);
+    void start();
+    void stop();
+    void acceptConnection();
+    void updateServerProgress();
+    void displayError(QAbstractSocket::SocketError socketError);
+    QString bindIpAddr();
+    void setOnlyOneClient();
+    quint8 getOnlyOneClient();
+
+    void on_startButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+private:
+    QTcpServer tcpServer;
+    QTcpSocket *tcpServerConnection;
+    qint64  TotalBytes;
+    qint64  bytesReceived;
+    qint64  fileNameSize;
+    qint64  bytesNeedRecv;
+    QString fileName;
+    QFile *localFile;
+    QByteArray inBlock;
+
+    qint8 OnlyOneClient; //仅一个客户端
 };
 
 #endif // MAINWINDOW_H
